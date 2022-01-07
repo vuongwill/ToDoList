ca5a14676ed6169982a256d2879b958752ef4f64
William <willvxd@gmail.com>
2022-01-06 17:11:55 -0800

    remove

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 document.querySelector('#userText').value = ''
 document.querySelector('#refresh').addEventListener('click', () => location.reload())
 document.querySelector('#enter').addEventListener('click', addNote)
+
+
 document.querySelector('#userTextClear').addEventListener('click', () => document.querySelector('#userText').value = '')
 
 
 function addNote() {
+    if (document.querySelector('#userText').value === '') {
+        alert(`Enter something in text box!`)
+    } else {
     let section = document.createElement('section')
     section.className = 'notes'
     section.innerText = document.querySelector('#userText').value
     document.querySelector('.notes').before(section)  
-    let button = document.createElement('button')
-    button.className = 'clearList'
-    button.innerHTML = 'Clear'
-    document.querySelector('.notes').append(button)
+    // let button = document.createElement('button')
+    // button.className = 'clearList'
+    // button.innerHTML = 'Clear'
+    // document.querySelector('.notes').append(button)
+    // document.querySelector('.clearList').addEventListener('click',() => document.querySelector('.notes').classList.add('hidden'))
 
     document.querySelector('#userText').value = ''
-    document.querySelector('.clearList').addEventListener('click', () => document.querySelector('.notes').classList.add('hidden'))
-
+    }
 }
 
